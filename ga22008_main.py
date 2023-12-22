a51b659f4074ea5f71f1b09d0f45540a0d3a87aa
Gunārs Ābeltiņš <gunars2003@gmail.com>
2023-12-22 10:11:11 +0200

    Fix naming

--- ga22008_main.py
+++ ga22008_main.py
@@ -1,7 +1,7 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from libadd import *
-from libmult import *
+from ga22008_libadd import *
+from ga22008_libmult import *
 a = 13
 b = 5
 print(plus(a,b))
